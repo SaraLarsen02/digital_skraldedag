9ec91edb95966f7e648f1d4cd9926eb82a0bd6ba
Line <lith0001@stud.kea.dk>
2024-09-17 08:45:37 +0200

    tailwind css og config rettelser

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,35 +1,11 @@
 // /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
-// };
-
-// const defaultTheme = require("tailwindcss/defaultTheme");
-
-// module.exports = {
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-// };
-
-/** @type {import('tailwindcss').Config} */
-import defaultTheme from "tailwindcss/defaultTheme";
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {fontFamily: {
+      poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+    },},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
