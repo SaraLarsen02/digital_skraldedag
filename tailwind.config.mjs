6d9f6443724e260989b6f74f8ce1428e5ab37a98
Sara <sala0003@stud.kea.dk>
2024-09-17 08:44:17 +0200

    Få fonte til at virke

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
-};
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+// };
 
 // const defaultTheme = require("tailwindcss/defaultTheme");
 
@@ -18,3 +18,18 @@ export default {
 //     },
 //   },
 // };
+
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
