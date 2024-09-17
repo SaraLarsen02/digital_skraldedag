79287c3452dea74f7c1cb7e8fd9a922d542ce282
Anika <anso0004@stud.kea.dk>
2024-09-17 09:20:42 +0200

    Farver og baggrund

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,21 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {fontFamily: {
-      poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
-    },},
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        blaa: "#0D2032",
+        moerkeblaa: "#001324",
+        lyseblaa: "#72B4F7",
+        lilla: "#9572F7",
+        green: "#72F79C",
+        rosa: "#F77282",
+        gul: "#F7CD72",
+        orange: "#FF8F60",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
